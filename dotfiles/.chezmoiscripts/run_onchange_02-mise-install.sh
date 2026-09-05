7757ce3e574cc9d889ac9e333d3a1573a12fc2fe
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2026-09-05 21:56:22 +0000

    fix: correct source path for mise config hash

--- dotfiles/.chezmoiscripts/run_onchange_02-mise-install.sh
+++ dotfiles/.chezmoiscripts/run_onchange_02-mise-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-# mise/config.toml hash: {{ include "dot_config/mise/config.toml" | sha256sum }}
+# mise/config.toml hash: {{ include "private_dot_config/mise/config.toml" | sha256sum }}
 
 set -eu
 
